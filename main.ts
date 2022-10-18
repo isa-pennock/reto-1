ac2f79a16a701d58051c5b8ed5f848ac0c5ad37a
isa-pennock <112428957+isa-pennock@users.noreply.github.com>
2022-10-17 22:32:36 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,57 +1,51 @@
-input.onButtonPressed(Button.A, function () {
-    Atlantis.move(-1)
+input.onPinPressed(TouchPin.P0, function () {
+    control.reset()
 })
-input.onButtonPressed(Button.AB, function () {
-    Atlantis.change(LedSpriteProperty.Y, 1)
-})
-input.onButtonPressed(Button.B, function () {
-    Atlantis.move(1)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    Atlantis.change(LedSpriteProperty.Y, -1)
-})
-let Atlantis: game.LedSprite = null
-Atlantis = game.createSprite(1, 1)
-let Joker = game.createSprite(randint(1, 4), 2)
-let Choki = game.createSprite(randint(1, 4), 3)
-let Loki = game.createSprite(randint(1, 4), 4)
+let Atlantis = game.createSprite(1, 1)
+let Joker = game.createSprite(randint(1, 4), randint(0, 4))
+let Choki = game.createSprite(randint(1, 4), randint(0, 4))
+let Loki = game.createSprite(randint(1, 4), randint(0, 4))
+game.setScore(0)
 basic.forever(function () {
-    while (Atlantis.isTouching(Choki)) {
-        Atlantis.delete()
+    while (input.buttonIsPressed(Button.AB)) {
+        Atlantis.change(LedSpriteProperty.Y, 1)
+        basic.pause(500)
+    }
+    while (input.buttonIsPressed(Button.A)) {
+        Atlantis.change(LedSpriteProperty.X, -1)
+        basic.pause(500)
+    }
+    while (input.buttonIsPressed(Button.B)) {
+        Atlantis.change(LedSpriteProperty.X, 1)
+        basic.pause(500)
+    }
+    while (input.logoIsPressed()) {
+        Atlantis.change(LedSpriteProperty.Y, -1)
+        basic.pause(500)
+    }
+    if (Atlantis.isTouching(Choki)) {
+        game.addScore(1)
         Choki.delete()
-        for (let index = 0; index < 3; index++) {
-            basic.showIcon(IconNames.No)
-            basic.clearScreen()
-            basic.pause(100)
-        }
-        music.playMelody("C D E F G A B C5 ", 120)
-        game.gameOver()
     }
-    while (Atlantis.isTouching(Joker)) {
-        Atlantis.delete()
+    if (Atlantis.isTouching(Joker)) {
+        game.addScore(1)
         Joker.delete()
-        for (let index = 0; index < 3; index++) {
-            basic.showIcon(IconNames.No)
-            basic.clearScreen()
-            basic.pause(100)
-        }
-        music.playMelody("C D E F G A B C5 ", 120)
-        game.gameOver()
     }
-    while (Atlantis.isTouching(Loki)) {
-        Atlantis.delete()
+    if (Atlantis.isTouching(Loki)) {
+        game.addScore(1)
         Loki.delete()
-        for (let index = 0; index < 3; index++) {
-            basic.showIcon(IconNames.No)
-            basic.clearScreen()
-            basic.pause(100)
-        }
-        music.playMelody("C D E F G A B C5 ", 120)
+    }
+    if (game.score() == 3) {
+        basic.clearScreen()
+        basic.showString("CONGRATS, VICTORY")
         game.gameOver()
     }
 })
 loops.everyInterval(3000, function () {
-    Joker.change(LedSpriteProperty.X, 1)
-    Choki.change(LedSpriteProperty.X, 1)
-    Loki.change(LedSpriteProperty.X, 1)
+    Joker.change(LedSpriteProperty.X, randint(1, 4))
+    Joker.change(LedSpriteProperty.Y, randint(0, 4))
+    Choki.change(LedSpriteProperty.X, randint(1, 4))
+    Choki.change(LedSpriteProperty.Y, randint(0, 4))
+    Loki.change(LedSpriteProperty.X, randint(1, 4))
+    Loki.change(LedSpriteProperty.Y, randint(0, 4))
 })
